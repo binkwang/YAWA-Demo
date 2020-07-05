155a421a4c42aec516fcaa403b0b12bd23cd1332
BinkWang <>
2020-07-06 11:44:28 +1200

    Remove Pods folder.

--- Pods/Target Support Files/Pods-YAWA-Tests-YAWA-DemoTests/Pods-YAWA-Tests-YAWA-DemoTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_YAWA_Tests_YAWA_DemoTests : NSObject
-@end
-@implementation PodsDummy_Pods_YAWA_Tests_YAWA_DemoTests
-@end
--- Pods/Target Support Files/Pods-YAWA-Tests-YAWA-DemoUITests/Pods-YAWA-Tests-YAWA-DemoUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_YAWA_Tests_YAWA_DemoUITests : NSObject
-@end
-@implementation PodsDummy_Pods_YAWA_Tests_YAWA_DemoUITests
-@end
